e54638c4340c29300f424469bffc7e183eef8b32
Ben Longaker <blongaker@Bens-MacBook-Pro-4.local>
2023-10-25 22:16:31 -0600

    added code to construct solution (but normalization DOES NOT WORK)

--- schrodinger.m
+++ schrodinger.m
@@ -1,12 +1,11 @@
 % Initialize variables
 N = 64; L = 20; [D,x] = cheb(N);
 x = x*L; D2 = D^2; D2 = D2(2:N,2:N);
-[~,w] = clencurt(N); w = w(2:N);
+[~,w] = clencurt(N);
 
 % Initial condition
 psi0 = exp(-0.1*x.^2); psi0(1)=0; psi0(N+1)=0;
-int_val = sum(w'.*psi0(2:N));
-psi0 = psi0/int_val;
+psi0 = psi0/(w*psi0);
 
 % Construct Hamiltonian operator
 V = x.^2; V = V(2:N); % Arbitrary potential
@@ -19,21 +18,28 @@ P = P(:,ind);
 % Columns of P are eigenfunctions and E is a vector of energy eigenvalues.
 
 % Normalize the eigenfunctions (each integrates to 1)
-for i=1:N-1
-    int_val = sum(w' .* P(:,i) .* conj(P(:,i)));
-    P(:,i) = P(:,i)/int_val;
-end
+% WHY IS THIS NOT WORKING?!?!?
+% for i=1:N-1
+%     int_val = w * ([0;P(:,i);0].*[0;conj(P(:,i));0]);
+%     P(:,i) = P(:,i)/int_val;
+% end
 
 % Compute c_j coefficients
-c = zeros(N-1,1); % represents the probabilities of finding particle energy
+c = zeros(N-1,1);
 for i=1:N-1
-    c(i) = sum(w' .* psi0(2:N) .* conj(P(:,i)));
+    c(i) = w * ([0;psi0(2:N);0] .* [0;conj(P(:,i));0]);
 end
 
 % Construct solution
 dt = 0.01; tf = 1; tvec = 0:dt:tf;
 tsteps = tf/dt;
 soln = zeros(N+1,tsteps+1);
+soln(:,1) = psi0;
+for i=1:tsteps
+    psi = sum(P*(c.*exp(1i*E*tvec(i))), 2);
+    soln(:,i+1) = [0;psi;0];
+end
+soln = soln';
 
 %%
 
@@ -44,3 +50,22 @@ for i=1:4
 end
 legend('$n=1$','$n=2$','$n=3$','$n=4$',Interpreter='latex')
 
+%%
+
+figure
+hold on
+xx = linspace(-1,1,1000);
+uu = polyval(polyfit(x,psi0,N),xx);
+plot(xx,uu,'LineWidth',2);
+scatter(x,psi0);
+
+%%
+
+figure
+for i=1:tsteps+1
+    plot(x, abs(soln(i,:)).^2, LineWidth=2);
+    ylim([-1,1]);
+    title(['$t=',num2str(tvec(i)),'$'],'Interpreter','latex')
+    drawnow;
+    pause(0.02);
+end
