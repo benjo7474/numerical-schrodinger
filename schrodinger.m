13ac065ad2b0db3b1058b58f8f49c4689c449d94
Ben Longaker <blongaker@Bens-MacBook-Pro-4.local>
2023-10-27 11:48:12 -0600

    cleaned up file a bit

--- schrodinger.m
+++ schrodinger.m
@@ -1,3 +1,5 @@
+%% Solve system
+
 close all;
 
 % Initialize variables
@@ -6,19 +8,21 @@ x = x*L; D2 = D^2; D2 = D2(2:N,2:N);
 [~,w] = clencurt(N); w = w*L;
 
 % Initial condition
-psi0 = exp(-0.1*x.^2); psi0(1)=0; psi0(N+1)=0;
+% psi0 = exp(-0.1*x.^2); psi0(1)=0; psi0(N+1)=0;
+psi0 = exp(-0.1*(x-5).^2) + exp(-0.1*(x+5).^2);
 psi0 = psi0/sqrt(w*psi0);
 % psi0 = psi0/sqrt(nonUniformTrap(x,psi0.*psi0));
 
 % Construct Hamiltonian operator
-V = zeros(N+1); V = V(2:N); % Arbitrary potential
+% V = 0.5*x.^2;  % Arbitrary potential
+V = zeros(N+1);
+V = V(2:N);
 H = -0.5*D2 + diag(V);
 
-% Compute eigenfunctions and eigenvalues, sort
+% Compute eigenfunctions and eigenvalues, sort by smallest eigenvalues
 [P,DD] = eig(H);
 [E,ind] = sort(diag(DD));
 P = P(:,ind);
-%%
 % Columns of P are eigenfunctions and E is a vector of energy eigenvalues.
 
 % Normalize the eigenfunctions (each squared integrates to 1)
@@ -29,7 +33,6 @@ for i=1:N-1
     P(:,i) = P(:,i)/sqrt(int_val);
 end
 
-%%
 % Compute c_j coefficients
 c = zeros(N-1,1);
 for i=1:N-1
@@ -39,7 +42,7 @@ for i=1:N-1
 end
 
 % Construct solution
-dt = 0.005; tf = 10; tvec = 0:dt:tf;
+dt = 0.005; tf = 5; tvec = 0:dt:tf;
 tsteps = tf/dt;
 soln = zeros(N+1,tsteps+1);
 soln(:,1) = psi0;
@@ -49,7 +52,7 @@ for i=1:tsteps
 end
 soln = soln';
 
-%%
+%% Plot first couple eigenfunctions (numerical)
 
 figure
 hold on
@@ -58,22 +61,25 @@ for i=1:4
 end
 legend('$n=1$','$n=2$','$n=3$','$n=4$',Interpreter='latex')
 
-% %%
-% 
-% figure
-% hold on
-% xx = linspace(-1,1,1000);
-% uu = polyval(polyfit(x,psi0,N),xx);
-% plot(xx,uu,'LineWidth',2);
-% scatter(x,psi0);
-% 
-%%
+%% Plot initial condition
 
 figure
+hold on
+xx = linspace(-1,1,1000);
+uu = polyval(polyfit(x,psi0,N),xx);
+plot(xx,uu,'LineWidth',2);
+scatter(x,psi0);
+
+%% Animiation of solution
+
+figure
+ymax = max(abs(soln).^2, [], 'all');
 for i=1:tsteps+1
     plot(x, abs(soln(i,:)).^2, LineWidth=2);
-    ylim([-.1,.1]);
-    title(['$t=',num2str(tvec(i)),'$'],'Interpreter','latex')
+    ylim([-0.1*ymax, 1.1*ymax]);
+    xlabel('$x$', 'Interpreter', 'latex', 'FontSize', 16)
+    ylabel('$p(x,t)$', 'Interpreter', 'latex', 'FontSize', 16)
+    title(['$t=',num2str(tvec(i), '%0.4f'),'$'],'Interpreter','latex', 'FontSize', 16)
     drawnow;
     pause(0.005);
 end
