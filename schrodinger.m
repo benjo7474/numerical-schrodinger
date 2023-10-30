3e001d4b15daa587ade011d0f2395639fa15d88e
Ben Longaker <blongaker@Bens-MacBook-Pro-4.local>
2023-10-30 13:55:15 -0600

    added eigenvalue plot and changed colors

--- schrodinger.m
+++ schrodinger.m
@@ -4,16 +4,16 @@ clear; close all;
 
 % Initialize variables
 N = 256; L = 10; [D,x] = cheb(N);
-x = x*L; D2 = D^2; D2 = D2(2:N,2:N);
+D = D/L; x = x*L; D2 = D^2; D2 = D2(2:N,2:N); 
 [~,w] = clencurt(N); w = w*L;
 
 % Specify initial condition and potential function
-psi0 = exp(-(x+5).^2/2).*exp(-1i*1.*x); % IC
-% psi0 = exp(-(x+5).^2/2);
+% psi0 = exp(-(x+5).^2/2).*exp(-1i*1.*x); % IC
+psi0 = exp(-(x).^2/2);
 % V = 30*exp(-0.5*(x-10).^2) + 30*exp(-0.5*(x+10).^2); % V
 % V = 0.5*x.^2;
-% V = zeros(N+1);
-V = 10*pot(x);
+V = zeros(N+1);
+% V = 0.1*pot(x);
 
 % Make sure IC is normalized
 psi0 = psi0/sqrt(w*(abs(psi0).^2));
@@ -46,7 +46,7 @@ for i=1:N-1
 end
 
 % Construct solution
-dt = 0.001; tf = 0.25; tvec = 0:dt:tf;
+dt = 0.05; tf = 100; tvec = 0:dt:tf;
 tsteps = tf/dt;
 soln = zeros(N+1,tsteps+1);
 soln(:,1) = psi0;
@@ -56,14 +56,27 @@ for i=1:tsteps
 end
 soln = soln';
 
+%% Defaults
+set(0,'defaulttextInterpreter','latex')
+set(0,'defaultAxesTickLabelInterpreter','latex'); 
+set(0,'defaultLegendInterpreter','latex');
+set(0,'defaultLineLineWidth',5);
+set(0,'defaultAxesFontSize',35)
+
 %% Plot first couple eigenfunctions (numerical)
 
 figure
 hold on
-for i=1:4
-    plot(x,[0;P(:,i);0],LineWidth=2);
-end
-legend('$n=1$','$n=2$','$n=3$','$n=4$',Interpreter='latex')
+
+plot(x,[0;P(:,1);0],Color='#f2ce18');
+plot(x,[0;P(:,2);0],Color='#38cfc2');
+plot(x,[0;P(:,3);0],Color='#f218e0');
+plot(x,[0;P(:,4);0],Color='r');
+
+legend('$n=1$','$n=2$','$n=3$','$n=4$')
+xlabel('$x$')
+ylabel('$\psi_n(x)$')
+title('Eigenfunctions for Infinite Square Well')
 
 %% Plot initial condition
 % 
@@ -81,6 +94,17 @@ title('Potential', 'FontSize', 16)
 xlabel('$x$', 'Interpreter', 'latex', 'FontSize', 16);
 ylabel('$V(x)$', 'Interpreter', 'latex', 'FontSize', 16);
 
+%% Plot eigenvalues
+figure
+n = 1:10;
+xx = linspace(0,10.5,1000);
+uu = pi^2/(8*L^2)*xx.^2;
+hold on
+plot(xx,uu,'Color','#38cfc2');
+scatter(n, E(n), 200, 'k', 'filled');
+legend('$n^2\pi^2/(8L^2)$','$E_n$ (numerical)','Location','northwest','FontSize',40)
+title('Eigenvalues for Infinite Square Well')
+
 %% Animiation of solution
 
 figure
@@ -91,11 +115,11 @@ for i=1:tsteps+1
     plot(x(2:N), 0.01*V, '-r')
     hold off
     ylim([-0.1*ymax, 1.1*ymax]);
-    xlabel('$x$', 'Interpreter', 'latex', 'FontSize', 16)
-    ylabel('$p(x,t)$', 'Interpreter', 'latex', 'FontSize', 16)
-    title(['Probability of particle location at $t=',num2str(tvec(i), '%0.4f'),'$'],'Interpreter','latex', 'FontSize', 16)
+    xlabel('$x$')
+    ylabel('$p(x,t)$')
+    title(['Probability of particle location at $t=',num2str(tvec(i), '%0.4f'),'$'])
     drawnow;
-%     pause(0.005);
+    % pause(0.005);
 end
 
 %% Complex animiation of solution
@@ -107,7 +131,7 @@ for i=1:tsteps+1
     plot3(x, imag(soln(i,:)),real(soln(i,:)), LineWidth=3);
 %     scatter3(x, real(soln(i,:)), imag(soln(i,:)),'filled');
     hold on
-    plot3(x(2:N), zeros(length(V)), 0.01*V,'-r',lineWidth=2);
+    % plot3(x(2:N), zeros(length(V)), 0.01*V,'-r',lineWidth=2);
     hold off
     ylim([-1.1*ymax, 1.1*ymax]);
     zlim([-1.1*ymax, 1.1*ymax]);
@@ -115,8 +139,8 @@ for i=1:tsteps+1
     ylabel('Imaginary', 'Interpreter', 'latex', 'FontSize', 16)
     zlabel('Real', 'Interpreter', 'latex', 'FontSize', 16)
     title(['Wave function at $t=',num2str(tvec(i), '%0.4f'),'$'],'Interpreter','latex', 'FontSize', 16)
-%     drawnow;
-    exportgraphics(gcf,'new3dcurvewpotential.gif','Append',true);
+    drawnow;
+    % exportgraphics(gcf,'new3dcurvewpotential.gif','Append',true);
 
 %     pause(0.005);
 end
